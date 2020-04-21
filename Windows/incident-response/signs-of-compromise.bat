d881927e9511130ff9c27df35c93e7f20382d389
Pradyumna Joshi <joshi.pradyumna@gmail.com>
2020-04-21 18:01:06 +0530

    Update signs-of-compromise.bat

--- Windows/incident-response/signs-of-compromise.bat
+++ Windows/incident-response/signs-of-compromise.bat
@@ -1,16 +1,14 @@
 ::## This script can be used by security administrators to check if the windows PC is compromised or not - ie. whether it contains malware or not.
-
 @echo off 
 set results_file="assessment.txt" 
-
 (
 echo "###################################################"
 echo "Getting current date and time information."
-echo "Date:"
+echo.
+echo "Date:" 
 date /T 
-echo "Time:"
+echo "Time:" 
 time /T 
-echo. 
 echo.
 echo "###################################################"
 
@@ -19,7 +17,6 @@ echo "Getting system information"
 echo. 
 systeminfo
 echo. 
-echo.
 echo "###################################################"
 
 echo "#################################################"
@@ -27,14 +24,12 @@ echo " Getting currently logged in users"
 echo. 
 echo %username% 
 echo.
-echo. 
 echo "#################################################"
 
 echo "#################################################"
-echo "Getting current user along with domain"
+echo "Getting information about current user and domain"
 echo. 
 whoami
-echo. 
 echo. 
 REM if you have microsoft sysinternals tools, make use of psloggedon.exe 
 REM psloggedon.exe -l
@@ -48,14 +43,12 @@ echo "Getting environment variables information."
 echo. 
 set 
 echo. 
-echo. 
 echo "###################################################"
 
 echo "###################################################"
 echo "Getting path information."
 echo. 
-echo %PATH% 
-echo. 
+echo "%PATH%" 
 echo. 
 echo "###################################################"
 
@@ -65,7 +58,6 @@ echo "Getting information about User Account Controls(UAC) - Enabled or Disabled
 echo " UAC limits what changes a program can make in Windows without permission from an administrator profile"
 echo "If the results of query are  'ENABLELUA   REG_DWORD    0x1' then part or all of the UAC components are on"
 echo. 
-echo. 
 REG QUERY HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System\ /v EnableLUA
 
 REM Another way:
@@ -73,8 +65,86 @@ REM reg query "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Poli
 REM if "%ERRORLEVEL%"=="0"  ECHO UAC disabled
 REM if "%ERRORLEVEL%"=="1"  ECHO UAC enabled
 echo. 
-echo. 
 echo "###################################################"
+
+echo "######################################################"
+echo "Getting shared folder(s) information."
+echo.
+net share
+echo.
+echo "Getting local network share information"
+net view \\127.0.0.1
+echo.
+echo "######################################################"
+
+echo "######################################################"
+echo "Getting password account policies information."
+echo.
+net accounts
+echo.
+echo "######################################################"
+
+echo "######################################################"
+echo "Getting windows services list"
+echo.
+sc query
+echo.
+echo "######################################################"
+
+
+echo "######################################################"
+echo "Getting information about windows services that are started during power up"
+echo.
+net start
+echo.
+echo "######################################################"
+
+echo "######################################################"
+echo "Getting information about process and its associated service" 
+echo.
+tasklist /svc 
+echo.
+echo "######################################################"
+
+echo "######################################################"
+echo " Getting information about all the running processes" 
+echo.
+tasklist /v 
+echo.
+echo "######################################################"
+
+echo "######################################################"
+echo "Enumerating user account information"
+echo.
+REM for /F "skip=1" %i in ('wmic useraccount get name') do net user %i >> %results_file%
+REM for /F "skip=1" %i in ('wmic /append:%results_file% useraccount get name') do net user %i
+for /F "skip=1" %%i in ('wmic useraccount get name') do net user %%i
+echo.
+echo "######################################################"
+
+echo "######################################################"
+echo "Enumerating group information"
+echo.
+REM for /F "delims=* tokens=1 skip=4" %i in ('net localgroup') do net localgroup %i >> %results_file%
+REM Stackoverflow thread - https://stackoverflow.com/questions/35262862/issues-with-spaces-in-for-loop-variable-batch-script
+REM for /F "delims=* tokens=1 skip=4" %i in ('net localgroup') do net localgroup %i
+for /F "delims=* tokens=1 skip=4" %%i in ('net localgroup') do net localgroup %%i
+echo.
+echo "######################################################"
+
+echo "######################################################"
+echo "Calculating SHA256 hashes of running processes. These can be checked against services like Virustotal"
+echo. 
+for /F %%i in ('wmic process where "ExecutablePath is not null" get ExecutablePath') do (certutil -hashfile %%i SHA256)
+echo.
+echo "######################################################"
+
+echo "######################################################"
+echo "Getting information about the DLL loaded by each running process"
+echo.
+for /f "tokens=*" %%a in ('wmic process get processid') do @(cmd /c tasklist /m /fi "pid eq %%a")
+echo.
+echo "######################################################"
 
 
 REM Credits:
@@ -83,85 +153,90 @@ REM * Windows priviledge escalation checks - https://github.com/codingo/OSCP-2/b
 
 ) > %results_file%
 
+REM The following information is obtained using Windows Management Instrumentation Console(WMIC)
 
 echo "##################################################"
 echo "Getting information about installed security patches, hotfixes etc."
 echo. 
-wmic /append:"assessment.txt" qfe get Caption,Description,HotFixID,InstalledOn /Format:CSV
-echo. 
+wmic /append:%results_file% qfe get Caption,Description,HotFixID,InstalledOn /Format:CSV
 echo. 
 echo "###################################################"
 
 echo "##################################################"
-echo "Getting mounted disk drives information."
+echo "Getting information about mounted disk drives"
 echo. 
-wmic /append:"assessment.txt" logicaldisk get caption 2> nul
-echo. 
+wmic /append:%results_file% logicaldisk get caption 2> nul
 echo. 
 echo "##################################################"
 
 echo "##################################################"
 echo "Getting information about registered Anti-virus program on the PC"
 echo. 
-wmic /append:"assessment.txt" /Node:localhost /Namespace:\\root\SecurityCenter2 Path AntiVirusProduct Get displayName /Format:CSV
-echo. 
+wmic /append:%results_file% /Node:localhost /Namespace:\\root\SecurityCenter2 Path AntiVirusProduct Get displayName /Format:CSV
 echo. 
 echo "##################################################"
 
 echo "##################################################"
 echo "Getting information about installed softwares."
 echo. 
-wmic /append:"assessment.txt" product get name,version,installdate,vendor /Format:CSV
-echo. 
+wmic /append:%results_file% product get name,version,installdate,vendor /Format:CSV
 echo. 
 echo "######################################################"
 
-# logical disks information
-wmic logicaldisk get name,freespace,systemname,size,volumename,filesystem
+echo "##################################################"
+echo "Getting logical disks information"
+echo.
+wmic /append:%results_file% logicaldisk get name,freespace,systemname,size,volumename,filesystem
+echo.
+echo "######################################################"
 
-# Disk information
-wmic diskdrive get name,size,mediatype
+echo "##################################################"
+echo "Getting Hard Disk information"
+echo.
+wmic /append:%results_file% diskdrive get name,size,mediatype
+echo.
+echo "######################################################"
 
-# computer information
-#wmic computersystem list /format:list
-wmic computersystem get domain,manufacturer,model,name,username,primaryownername,numberofprocessors,systemtype
+echo "##################################################"
+echo "Getting computer Hardware Components information"
+REM wmic computersystem list /format:list
+echo.
+wmic /append:%results_file% computersystem get domain,manufacturer,model,name,username,primaryownername,numberofprocessors,systemtype
+echo.
+echo "######################################################"
 
-# product list
-wmic product get description,installdate,name,vendor,version
+echo "##################################################"
+echo "Getting installed product(s) information"
+echo.
+wmic /append:%results_file% product get description,installdate,name,vendor,version
+echo.
+echo "######################################################"
 
-# shared folders
-net share
-net view \\127.0.0.1
+echo "######################################################"
+echo "Getting user account(s) information"
+echo.
+wmic /append:%results_file% useraccount list full
+echo.
+echo "######################################################"
 
-# password account policies
-net accounts
+echo "######################################################"
+echo "Getting windows services list using wmic"
+echo.
+wmic /append:%results_file% service get displayname,pathname,startname,state,startmode,servicetype
+echo.
+echo "######################################################"
 
-# user account details
- wmic useraccount list full
+echo "######################################################"
+echo "Getting process list using wmic" 
+echo.
+REM wmic process list full 
+wmic /append:%results_file% process get name,processid,parentprocessid,executablepath,installdate,virtualsize,executionstate 
+echo.
+echo "######################################################"
 
-# Enumerate user information
-#for /F "skip=1" %i in ('wmic useraccount get name') do net user %i >> c:\user.txt
-for /F "skip=1" %i in ('wmic useraccount get name') do net user %i
-
-# Enumerate group information
-for /F "delims=* tokens=1 skip=4" %i in ('net localgroup') do net localgroup %i >> c:\groups.txt
-
-# Windows Service list
-sc query
-# Windows service list using wmic
-wmic service get displayname,pathname,startname,state,startmode,servicetype
-
-# Windows services started during power up
-net start
-
-# Process with associated service 
-tasklist /svc 
-
-# all running process list 
-tasklist /v 
-
-# process list using wmic 
-wmic process list full 
-wmic process get name,processid,parentprocessid,executablepath,installdate,virtualsize,executionstate 
-
-::more-settings-to-be-added...
+echo "######################################################"
+echo "Getting full command line information of the running processes using wmic" 
+echo.
+wmic /append:%results_file% process where "commandline is not null and commandline!=''" get name,commandline
+echo.
+echo "######################################################"
